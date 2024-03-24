001c750b82c1a95364ac3c1d5533e11f6a1a82a2
Muhammad Sani Ibrahim <87580367+Saneeitas@users.noreply.github.com>
2024-03-24 13:46:33 +0100

    update ninja

--- src/ninjas/ninjas.service.ts
+++ src/ninjas/ninjas.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@nestjs/common';
+import { CreateNinjaDto } from './dto/create-ninja.dto';
+import { UpdateNinjaDto } from './dto/update-ninja.dto';
 
 @Injectable()
 export class NinjasService {
@@ -13,5 +15,36 @@ export class NinjasService {
         }
         return this.ninjas;
     }
+
+    getNinja(id: number){
+        const ninja = this.ninjas.find((ninja)=> ninja.id === id);
+
+        if(!ninja){
+            throw new Error('ninja not found!')
+        }
+        return ninja
+    }
+
+    createNinja(createNinjaDto: CreateNinjaDto){
+        const newNinja = {
+            ...CreateNinjaDto,
+            id: Date.now(),
+        };
+        this.ninjas.push(newNinja)
+
+        return newNinja
+    }
+
+    updateNinja(id: number, updateNinjaDto: UpdateNinjaDto){
+        this.ninjas = this.ninjas.map((ninja)=>{
+            if(ninja.id === id){
+                return {...ninja, ...updateNinjaDto}
+            }
+            return ninja
+        })
+        return this.getNinja(id)
+    }
+
+    
 
 }

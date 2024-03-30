aa7c71db3bc5a684b238f84d7d2b4b4fed5994c6
Your Name <muhdsanee87@gmail.com>
2024-03-30 17:37:24 +0100

    IsEnum validator

--- src/ninjas/dto/create-ninja.dto.ts
+++ src/ninjas/dto/create-ninja.dto.ts
@@ -1,7 +1,9 @@
-import { MinLength } from "class-validator";
+import { IsEnum, MinLength } from "class-validator";
 
 export class CreateNinjaDto {
     @MinLength(3)
     name: string;
+
+    @IsEnum(['stars', 'nunchucks'], {message: 'Use correct option'})
     weapon: 'starts' | 'nunchucks';
 }

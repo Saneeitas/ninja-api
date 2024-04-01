58cccf0efe1eda7ff4659fd8c5f9c4b8952aebf5
Your Name <muhdsanee87@gmail.com>
2024-04-01 23:29:28 +0100

    link mongoose module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppService } from './app.service';
 import { NinjasModule } from './ninjas/ninjas.module';
 import { UsersModule } from './users/users.module';
+import { NinjaSchema } from './ninjas/schema/schema';
 
 @Module({
-  imports: [NinjasModule, UsersModule],
+  imports: [
+    NinjasModule,
+    UsersModule,
+    MongooseModule.forRoot('mongodb://localhost:27017/ninjas-app'),
+    MongooseModule.forFeature([{ name: 'Ninja', schema: NinjaSchema }]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
